cad09817bc128c4a84b5fa79c004cc27ae476187
tniranjan1 <tniranj1@gmail.com>
2021-07-12 13:21:36 +0200

    Conda check debug 4

--- workflow/run_master_workflow.sh
+++ workflow/run_master_workflow.sh
@@ -16,13 +16,12 @@ RRBS_made=$(conda env list | cut -f1 -d " " | grep 'RRBS' | wc -l)
 if [ "$RRBS_made" -eq "0" ]
 then
   ## yes, activate RRBS env
-  conda activate RRBS
-  echo "RRBS conda environment already created."
-else
-  ## no, install and activate RRBS env
-  conda init bash
   conda create -n RRBS --prefix ./envs python=3.8 snakemake
   echo "Creating RRBS conda environment."
   conda activate RRBS
+else
+  ## no, install and activate RRBS env
+  conda activate RRBS
+  echo "RRBS conda environment already created."
 fi
 echo "Activating RRBS conda environment."
