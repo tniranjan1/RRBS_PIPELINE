c527dd0e189152c490b70ed2210ed80b0dbb6723
tniranjan1 <tniranj1@gmail.com>
2021-07-12 15:49:06 +0200

    Conda check debug 5

--- workflow/run_master_workflow.sh
+++ workflow/run_master_workflow.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## This is a master workflow initiator script.
+## This is the only script that needs to be run.
+## This script will check conda installations
+## and then start the snakemake workflow.
+
 echo "Starting RRBS workflow."
 ## Is conda installed?
 if ! type conda > /dev/null
@@ -9,6 +14,8 @@ then
 else
   condaver=$(conda --version)
   echo "$condaver installed." | sed 's/conda/Conda/'
+  CONDA_BASE="$(conda info --base)/etc/profile.d/conda.sh"
+  source $CONDA_BASE
 fi
 
 ## Is RRBS conda environment alrady created?
@@ -16,7 +23,7 @@ RRBS_made=$(conda env list | cut -f1 -d " " | grep 'RRBS' | wc -l)
 if [ "$RRBS_made" -eq "0" ]
 then
   ## yes, activate RRBS env
-  conda create -n RRBS --prefix ./envs python=3.8 snakemake
+  conda create -n RRBS python=3.8 snakemake
   echo "Creating RRBS conda environment."
   conda activate RRBS
 else
