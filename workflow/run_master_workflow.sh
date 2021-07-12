a5c76a1672280abce22cc2d293288d1718ef2565
tniranjan1 <tniranj1@gmail.com>
2021-07-12 13:02:56 +0200

    Conda check debug 1

--- workflow/run_master_workflow.sh
+++ workflow/run_master_workflow.sh
@@ -2,13 +2,14 @@
 
 echo "Starting RRBS workflow"
 ## Is conda installed?
-{
+if ! type conda > /dev/null
+  then
+    echo "Conda not installed or not aliased. Exiting."
+    exit -1
+else
   condaver=$(conda --version)
-} || {
-  "Error: Conda is not installed."
-  set -e
-}
-echo "$condaver installed."
+  echo "$condaver installed" | sed 's/conda/Conda/'
+fi
 
 ## Is RRBS conda environment alrady created?
   ## yes, activate RRBS env
