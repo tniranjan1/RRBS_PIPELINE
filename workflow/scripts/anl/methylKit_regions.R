6d97c0eb73530e9eda105967154e3afe2cce7c9b
tniranjan1 <tniranj1@gmail.com>
2023-05-25 12:13:35 +0200

    debug methylkit tiling 1

--- workflow/scripts/anl/methylKit_regions.R
+++ workflow/scripts/anl/methylKit_regions.R
@@ -11,7 +11,15 @@ sink(log, type = "message", append=TRUE)
 # load libraries
 library(methylKit)
 
+# Log input parameters (sanity check)
+sprintf("Input path:  %s\n", input_path)
+sprintf("Output path: %s\n", output_path)
+sprintf("Sample name: %s\n", sample_name)
+sprintf("Context:     %s\n", context)
+sprintf("Log path:    %s\n", log_path)
+
 # read methylation data from input_path file
+cat("Reading methylation data from input path.\n")
 rawMethylObj <- methRead(location=input_path,
                          sample.id=sample_name,
                          assembly="hg38",
@@ -20,14 +28,17 @@ rawMethylObj <- methRead(location=input_path,
                          pipeline='bismarkCoverage')
 
 # tile the methylation data
+cat("Tiling methylation data.\n")
 tiles <- tileMethylCounts(rawMethylObj, win.size=500, step.size=250, cov.bases=5)
 
 # read the header line of the input_path file
+cat("Reading header from input path.")
 gz_file <- gzfile(input_path, "r")
 header <- readLines(gz_file, n = 1)
 close(gz_file)
 
 # organize the tiled methylation data into bismarkCoverage format
+cat("Organizing tiled methylation data.")
 out <- data.frame(chr=tiles$chr,
                   start=tiles$start,
                   end=tiles$end,
@@ -36,6 +47,7 @@ out <- data.frame(chr=tiles$chr,
                   T=tiles$numTs)
 
 # write the header and tiled data to file
+cat("Writing tiled methylation data to file.\n")
 gz_file <- gzfile(output_path, "w")
 cat(header, file = gz_file, sep = "\n")
 write.table(out, gz_file, sep = "\t", row.names=F, col.names=F, quote=F, append=T)
