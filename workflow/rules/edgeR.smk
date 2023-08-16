3a8f6f6483b1dabf12114116dccaf79e91cc2a8d
tniranjan1 <tniranj1@gmail.com>
2023-08-16 14:28:26 +0200

    get trackless files

--- workflow/rules/edgeR.smk
+++ workflow/rules/edgeR.smk
@@ -11,14 +11,25 @@ rule run_methylKit:
 
 #----------------------------------------------------------------------------------------------------------------------#
 
+# Make temporary trackless file for input into edgeR
+rule trackless:
+    input: "{prefix}.bedGraph.gz"
+    output: temp("{prefix}.detrack.gz")
+    conda: f"{workflow_dir}/envs/edgeR.yaml"
+    threads: 1
+    shell: "exec > /dev/null; exec 2> /dev/null; zcat {input} | tail -n +2 | bgzip -c > {output}"
+
+#----------------------------------------------------------------------------------------------------------------------#
+
 # Perform edgeR analysis of methylation
 rule run_edgeR:
-  input: expand(results_dir + "/rrbs_samples/methylation_calls/samples/methylKit_{sample}.{repeats}_{context}.bedGraph.gz", sample=set_options['rrbs_samples'], repeats=repeats, context='CpG')
+  input: expand(results_dir+"/rrbs_samples/methylation_calls/samples/methylKit_{sample}.{repeats}_{context}.detrack.gz",
+                sample=set_options['rrbs_samples'], repeats=repeats, context='CpG')
   output: results_dir + "/rrbs_samples/methylation_calls/samples/edgeR.tmp"
   threads: 12
   conda: f"{workflow_dir}/envs/edgeR.yaml"
   log: results_dir + "/rrbs_samples/methylation_calls/samples/.edgeR.tmp.log"
-  shell: "exec > {log}; exec 2> {log}; echo run > {output}"
+  shell: "exec > {log}; exec 2> {log}; exit 1"
   #script: f"{workflow_dir}/scripts/anl/edgeR.R"
 
 #----------------------------------------------------------------------------------------------------------------------#
--- workflow/scripts/anl/edgeR.R
+++ workflow/scripts/anl/edgeR.R
@@ -31,16 +31,45 @@ comp_shortcut <- list(
   "Y" = c("FTLD-TDP43-A", "FTLD-TDP43-B", "FTLD-TDP43-C", "FTLD-TDP43-C9-Positive", "FTLD-TDP43-GRN")
 )
 
+# get sample names to remove
+removal <- scan(remove_samples, what=character(), sep="\n")
+removal <- grep('^#', removal, value=T, invert=T)
+removal <- as.vector(sapply(removal, function(x) { strsplit(x, "\t")[[1]][1] }))
+removal <- gsub('s_', '', removal)
+removal <- grep(tissue, removal, value=T)
+for(n in names(comp_shortcut)) removal <- gsub(paste('^', n, '_', sep=''), '', removal)
+removal <- gsub('^Ctrl_', '', removal)
+removal <- gsub(paste('_', tissue, '$', sep=''), '', removal)
+
 # process covariate data
 covariates <- read.table(covariate_path, stringsAsFactors=F, header=T)
 covariates <- subset(covariates, Tissue==tissue)
+## remove outlier samples
+for(sample in removal) covariates <- subset(covariates, SampleID != sample)
 
 # sample names
 control_samples <- subset(covariates, SampleGroup=='Ctrl')$SampleID
+comp_samples <- sapply(comp_shortcut[[comparison_group]], function(x) {subset(covariates, SampleGroup==x)$SampleID})
+comp_samples <- as.vector(unlist(comp_samples))
+
+# split covariate data
+rownames(covariates) <- covariates$SampleID
+control_covariates <- covariates[control_samples,]
+comparison_covariates <- covariates[comp_samples,]
 
 # process deconvolution data
-  # check do_deconvolution
+  if(do_deconvolution == 'with_deconvolution')
+  {
+    decon_df <- read.table(deconvolution_path, header=T, stringsAsFactors=F)
+    decon_df <- subset(decon_df, grepl(tissue, group))
+    decon_df <- subset(decon_df, method=='nnls' & references=='all' & CpGsites=='all')
+    special_split <- function(x) { gsub(paste(decon_df[x,'group'], '.', sep=''), '', decon_df[x,'sample_name']) }
+    cells <- c('Neuron', 'Oligodendrocyte', 'Other')
+    rownames(decon_df) <- sapply(seq(nrow(decon_df)), special_split)
+    control_covariates[control_samples, cells] <- decon_df[gsub('[_-]', '.', control_samples, perl=T), cells]
+    comparison_covariates[comp_samples, cells] <- decon_df[gsub('[_-]', '.', comp_samples, perl=T), cells]
+  }
 
-# isolate methylkit file paths matching control
-
-# isolate methylkit file paths matching comparison group
+# isolate methylkit file paths matching control and comparison group
+control_files <- as.vector(sapply(control_samples, function(x) { grep(x, methylation_files, value=T) }))
+comparison_files <- as.vector(sapply(comp_samples, function(x) { grep(x, methylation_files, value=T) }))
