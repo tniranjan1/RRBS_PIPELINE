02e01edf2643841f703e8c8c4070e614d5035fbf
tniranjan1 <tniranj1@gmail.com>
2023-08-16 11:29:24 +0200

    update edgeR

--- workflow/scripts/anl/edgeR.R
+++ workflow/scripts/anl/edgeR.R
@@ -7,7 +7,11 @@ covariate_path <- snakemake@input[['covariate_path']]
 deconvolution_path <- snakemake@input[['deconvolution_path']]
 methylation_files <- snakemake@input[['methylation_files']]
 control_group <- snakemake@params[['control']]
+comparison_group <- snakemake@params[['comparison']]
 output_file <- snakemake@output[['path']]
-comparison_group <- %function of output file name %
 
-# 
+# load enhancer data
+enhancer_gene <- snakemake@input[['enhancer_hs']]
+enhancer_all <- snakemake@input[['enhancer_sp']]
+load(enhancer_all)
+load(enhancer_gene)
